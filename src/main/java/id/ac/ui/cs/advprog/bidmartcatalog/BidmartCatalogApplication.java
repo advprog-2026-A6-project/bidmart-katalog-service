4dfcaf6b5724a2b431e45a02fe5f95b46ee66da7
Rainmo0 <aunknown145@gmail.com>
2026-02-19 23:39:35 +0700

    komet pertama

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmartcatalog/BidmartCatalogApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartCatalogApplication.class, args);
+    }
+
+}
