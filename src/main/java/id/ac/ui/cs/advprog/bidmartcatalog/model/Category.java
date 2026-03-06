a78197dc74a7d65da479231797eb66325c473101
Rainmo0 <aunknown145@gmail.com>
2026-03-06 16:29:58 +0700

    fix category error

--- src/main/java/id/ac/ui/cs/advprog/bidmartcatalog/model/Category.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartcatalog/model/Category.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.bidmartcatalog.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -23,8 +25,10 @@ public class Category {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
+    @JsonBackReference // Prevents the parent from being serialized inside the child
     private Category parent;
 
     @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @JsonManagedReference // Allows the children to be serialized inside the parent
     private List<Category> children = new ArrayList<>();
 }
