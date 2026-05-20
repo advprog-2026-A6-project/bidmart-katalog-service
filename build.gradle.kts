abb42c52a4917d68a2c764adbf09a4fcf603cd8d
Rainmo0 <aunknown145@gmail.com>
2026-05-20 10:56:19 +0700

    change build fix workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "5.0.0.4638"
 }
 
 group = "id.ac.ui.cs.advprog"
