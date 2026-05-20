8358d46ac44c08a62b0cf5b2cfa6ab566b432d2a
Rainmo0 <aunknown145@gmail.com>
2026-05-20 15:58:53 +0700

    fix code

--- src/main/java/id/ac/ui/cs/advprog/bidmartcatalog/service/ListingService.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartcatalog/service/ListingService.java
@@ -62,6 +62,11 @@ public class ListingService {
                 .toList();
     }
 
+    public Listing getListingById(UUID id) {
+        return listingRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Listing not found"));
+    }
+
     public ListingDTO updateListing(UpdateListingRequest request, UUID listingId) {
         Listing listing = listingRepository.findById(listingId)
                 .orElseThrow(() -> new RuntimeException("Listing not found"));
